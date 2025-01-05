8200f2a10ac4f9f4a107c659ba32729e33315911
DnG-Crafts <140936256+DnG-Crafts@users.noreply.github.com>
2025-01-05 14:25:30 +0800

    Initial

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ if [ -e '/usr/share/moonraker' ]; then
 fi
 
 echo "Installing new moonraker..."
-cp -r moonraker /usr/share/moonraker
+cp -r K2-Camera-main/moonraker /usr/share/moonraker
 
 echo "Install camera support..."
-cp camera.html /usr/share/frontend/camera.html
-cp index.html /usr/share/frontend/index.html
-cp favicon.ico /usr/share/frontend/favicon.ico
-cp mylogo.png /usr/share/frontend/mylogo.png
-cp camera.html /usr/share/fluidd/camera.html
+cp K2-Camera-main/camera.html /usr/share/frontend/camera.html
+cp K2-Camera-main/index.html /usr/share/frontend/index.html
+cp K2-Camera-main/favicon.ico /usr/share/frontend/favicon.ico
+cp K2-Camera-main/mylogo.png /usr/share/frontend/mylogo.png
+cp K2-Camera-main/camera.html /usr/share/fluidd/camera.html
 
 echo "Complete."
