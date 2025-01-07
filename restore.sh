fce25b51e65b93ae5383e9743e8a3b5c3d212ca0
DnG-Crafts <140936256+DnG-Crafts@users.noreply.github.com>
2025-01-07 13:11:39 +0800

    Initial

--- restore.sh
+++ restore.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+if [[ $(sh -c 'exec /etc/init.d/moonraker status') == *"running"* ]]; then
+sh -c 'exec /etc/init.d/moonraker stop'
+fi
 if [ -e '/usr/share/moonraker_backup' ]; then
     echo "Restore original moonraker from 'moonraker_backup'"
 	if [ -e '/usr/share/moonraker' ]; then
@@ -7,6 +10,7 @@ if [ -e '/usr/share/moonraker_backup' ]; then
 		rm -r /usr/share/moonraker
     fi
 	cp -r /usr/share/moonraker_backup /usr/share/moonraker
+	sh -c 'exec /etc/init.d/moonraker start'
 	echo "Complete."
 else	
 	echo "No backup found, nothing to do here."
